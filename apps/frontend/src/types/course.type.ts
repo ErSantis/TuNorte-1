c5f8af1ecc22983ec62bf28d624571091fb277b9
Elkin <ersantis@uninorte.edu.co>
2025-04-08 10:53:52 -0500

    Add CourseType definitions for course information, tasks, locations, and schedules

---
+++ apps/frontend/src/types/course.type.ts
@@ -0,0 +1,37 @@
+import { ProfessorType } from "./professor.type";
+
+export type CourseInfoType = {
+    name: string;
+    nrc: string;
+    professors: ProfessorType[];
+    nameDept: string;
+}
+
+
+export type CourseTaskType = {
+    idtask: number;
+    title: string;
+    description: string;
+    enddate: string;
+    status: number;
+};
+
+export type CourseLocationType = {
+    name: string;
+    latitude: string;
+    longitude: string;
+};
+
+export type CourseScheduleType = {
+    day: string;
+    starttime: string;
+    endtime: string;
+    location: CourseLocationType;
+}
+
+
+export type CourseType = {
+    info: CourseInfoType;
+    schedules: CourseScheduleType[];
+    tasks: CourseTaskType[];
+};
