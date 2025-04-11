d10f29fcdf2c0f8e3404ce454319f652b695cabe
Elkin <ersantis@uninorte.edu.co>
2025-04-11 00:25:16 -0500

    format code

--- apps/backend/src/controllers/task.controller.ts
+++ apps/backend/src/controllers/task.controller.ts
@@ -1,61 +1,62 @@
-import { Request, Response } from "express";
-import { completeTaskByid, createTaskByNrc, deleteTaskById, editTaskById } from "../services/task.service";
+import { Request, Response } from 'express';
+import {
+  completeTaskByid,
+  createTaskByNrc,
+  deleteTaskById,
+  editTaskById,
+} from '../services/task.service';
 
 export const completeTask = async (req: Request, res: Response) => {
+  const { idtask } = req.params;
 
-    const { idtask } = req.params;
+  const parsedId = parseInt(idtask, 10);
 
-
-    const parsedId = parseInt(idtask, 10);
-
-    try {
-        const result = await completeTaskByid(parsedId);
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error completing task' });
-    }
-}
+  try {
+    const result = await completeTaskByid(parsedId);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error completing task' });
+  }
+};
 
 export const editTask = async (req: Request, res: Response) => {
-    const { idtask } = req.params;
-    const data = req.body;
+  const { idtask } = req.params;
+  const data = req.body;
 
-    const parsedId = parseInt(idtask, 10);
+  const parsedId = parseInt(idtask, 10);
 
-    try {
-        const result = await editTaskById(parsedId, data);
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error editing task' });
-    }
-}
+  try {
+    const result = await editTaskById(parsedId, data);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error editing task' });
+  }
+};
 
 export const deleteTask = async (req: Request, res: Response) => {
-    const { idtask } = req.params;
+  const { idtask } = req.params;
 
-    const parsedId = parseInt(idtask, 10);
+  const parsedId = parseInt(idtask, 10);
 
-    try {
-        const result = await deleteTaskById(parsedId);
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error deleting task' });
-    }
-}
-
+  try {
+    const result = await deleteTaskById(parsedId);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error deleting task' });
+  }
+};
 
 export const createTask = async (req: Request, res: Response) => {
+  const data = req.body;
 
-    const data = req.body;
-
-    try {
-        const result = await createTaskByNrc(data);
-        res.status(201).json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error creating task' });
-    }
-}
+  try {
+    const result = await createTaskByNrc(data);
+    res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating task' });
+  }
+};
--- apps/backend/src/services/subject.service.ts
+++ apps/backend/src/services/subject.service.ts
@@ -1,28 +1,28 @@
 // src/services/student.service.ts
-import { AppDataSource } from "../db";
-import { CoursesRegister } from "../entities/CourseRegister";
+import { AppDataSource } from '../db';
+import { CoursesRegister } from '../entities/CourseRegister';
 
 export const getCoursesByStudentId = async (idStudent: string) => {
   const repo = AppDataSource.getRepository(CoursesRegister);
 
   const results = await repo.find({
     where: {
-      student: { idstudent: idStudent }
+      student: { idstudent: idStudent },
     },
     relations: {
       student: true,
       course: {
         subject: {
-          department: true
+          department: true,
         },
-        professor: true
-      }
-    }
+        professor: true,
+      },
+    },
   });
 
-  return results.map(reg => ({
+  return results.map((reg) => ({
     NRC: reg.course.nrc,
     Name: reg.course.subject.name,
-    idDept: reg.course.subject.department.iddept
+    idDept: reg.course.subject.department.iddept,
   }));
 };
