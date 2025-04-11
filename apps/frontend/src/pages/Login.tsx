8d681dbb9132594726a02ba5b0ae8d3f43fde594
Elkin <ersantis@uninorte.edu.co>
2025-04-10 23:39:55 -0500

    fix: update Login component to use correct import for useLoginUser and improve login handling

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,14 +1,38 @@
 import "../styles/Login.css";
 import LoginForm from "../components/login/LoginForm";
-import { useLoginUser } from "../hooks/useLoginUser";
+import { useLoginUser } from "../hooks/useUser";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAuth } from "../context/AuthContext";
 
-const Login:  React.FC = () => {
-  const { mutate: login } = useLoginUser();
+const Login: React.FC = React.memo(() => {
 
+  const navigate = useNavigate();
+  const {mutate: loginMuatation } = useLoginUser();
+  const {login :LoginUser} = useAuth()
   const handleLogin = (username: string, password: string) => {
-    login({ username, password });
+    loginMuatation(
+      { username, password },
+      {
+        onSuccess: (data) => {
+          // Call the login function from AuthContext, save it in the context
+          LoginUser(data);
+          // Redirect to another page on success
+          navigate("/home");
+        },
+        onError: (error) => {
+          // Set error message to display CustomAlert
+            toast.error(`🚨 ${error.message}`, {
+            position: "top-center",
+            autoClose: 4000,
+            closeOnClick: false,
+            closeButton: false,
+            });
+        },
+      }
+    );
   };
-
   return (
     <div className="wrapper">
       <div className="formContent">
@@ -17,5 +41,5 @@ const Login:  React.FC = () => {
       </div>
     </div>
   );
-};
+});
 export default Login;
